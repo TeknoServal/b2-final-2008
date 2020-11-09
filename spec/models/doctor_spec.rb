7fa3ae79ce7f43f8adac6e82e5eb58fe3896442e
Aidan Murray <aidanhansm@gmail.com>
2020-11-09 09:33:36 -0700

    Add doctor spec

---
+++ spec/models/doctor_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Doctor, type: :model do
+  describe 'validations' do
+    it do
+      should validate_presence_of :name
+      should validate_presence_of :specialty
+      should validate_presence_of :university
+    end
+  end
+
+  describe 'relationships' do
+    it do
+      should belong_to :hospital
+      should have_many(:patients).through(:patient_doctors)
+    end
+  end
+end
