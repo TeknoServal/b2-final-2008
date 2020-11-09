40f0a20f2abcb137830a6068887ef702e71ea07b
Aidan Murray <aidanhansm@gmail.com>
2020-11-09 10:25:43 -0700

    US 1: Doctor show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -2,27 +2,28 @@ require 'rails_helper'
 
 describe 'Doctor show page' do
   before :each do
-    @hospital = Hospital.new(name: 'charles memorial hospital')
+    @hospital = Hospital.create!(name: 'charles memorial hospital')
 
-    @doctor = @hospital.doctors.new(name: 'Francis Harvestbringer', specialty: 'nasal spray', university: 'Harvard')
+    @doctor = @hospital.doctors.create!(name: 'Francis Harvestbringer', specialty: 'nasal spray', university: 'Harvard')
 
-    @patient1 = @doctor.patients.new(name: 'harry', age: 19)
-    @patient1 = @doctor.patients.new(name: 'david', age: 24)
-    @patient1 = @doctor.patients.new(name: 'guineveve', age: 31)
+    @patient1 = @doctor.patients.create!(name: 'harry', age: 19)
+    @patient1 = @doctor.patients.create!(name: 'david', age: 24)
+    @patient1 = @doctor.patients.create!(name: 'guineveve', age: 31)
   end
 
   describe 'visit the show page' do
-    visit "/doctors/#{@doctor.id}"
+    it do
+      visit "/doctors/#{@doctor.id}"
 
-    expect(page).to have_content(@doctor.name)
-    expect(page).to have_content(@doctor.specialty)
-    expect(page).to have_content(@doctor.university)
+      expect(page).to have_content(@doctor.name)
+      expect(page).to have_content(@doctor.specialty)
+      expect(page).to have_content(@doctor.university)
 
-    expect(page).to have_content(@doctor.hospital)
+      expect(page).to have_content(@doctor.hospital.name)
 
-    @doctor.patients.each do |patient|
-      expect(page).to have_content(patient.name)
+      @doctor.patients.each do |patient|
+        expect(page).to have_content(patient.name)
+      end
     end
-
   end
 end
